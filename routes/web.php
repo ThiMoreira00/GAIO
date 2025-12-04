b0ddd448b8b0232c5c268ddfea37fb0fb3efe4db
Thiago Moreira <129688942+ThiMoreira00@users.noreply.github.com>
2025-12-04 11:25:36 -0300

    🔧 fix: Roteamento consertado

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Router::get('/documento', 'ValidacaoController@exibirDocumento');
 Router::post('/documento', 'ValidacaoController@validarDocumento');
 
 // Carteirinha
-Router::get('/carteirinha', 'ValidacaoController@exibirCarteirinha');
-Router::post('/carteirinha', 'ValidacaoController@validarCarteirinha');
+// Router::get('/carteirinha', 'ValidacaoController@exibirCarteirinha'); // TODO: Implementar
+// Router::post('/carteirinha', 'ValidacaoController@validarCarteirinha'); // TODO: Implementar
 
 /*  ========================================
  * ROTAS INTERNAS
@@ -44,6 +44,45 @@ Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void 
     Router::get('/inicio', 'PainelController@exibirIndex');
     Router::get('/configuracoes', 'ContaController@exibirConfiguracoes'); // Configurações
     Router::get('/notificacoes', 'NotificacaoController@exibirIndex'); // Notificações
+
+    // Rotas de Turmas - Aluno
+    Router::grupo(['middleware' => new ControleAcessoMiddleware('ALUNO')], function (): void {
+        Router::get('/turmas', 'TurmaController@exibirIndexAluno');
+        Router::get('/turmas/{id}', 'TurmaController@exibirTurmaAluno', ['id' => '[0-9]+']);
+        
+        Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
+            Router::get('/turmas/filtrar', 'TurmaController@filtrarTurmasAluno');
+            Router::get('/turmas/{id}/dados', 'TurmaController@obterTurma', ['id' => '[0-9]+']);
+            Router::get('/turmas/dados-filtros', 'TurmaController@obterDadosFiltros');
+            Router::get('/turmas/estatisticas', 'TurmaController@obterEstatisticas');
+        });
+    });
+
+    // Rotas de Turmas - Professor
+    Router::grupo(['middleware' => new ControleAcessoMiddleware('PROFESSOR')], function (): void {
+        Router::get('/turmas', 'TurmaController@exibirIndexProfessor');
+        Router::get('/turmas/{id}', 'TurmaController@exibirTurmaProfessor', ['id' => '[0-9]+']);
+        
+        Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
+            Router::get('/turmas/filtrar', 'TurmaController@filtrarTurmasProfessor');
+            Router::get('/turmas/{id}/dados', 'TurmaController@obterTurma', ['id' => '[0-9]+']);
+            Router::get('/turmas/dados-filtros', 'TurmaController@obterDadosFiltros');
+            Router::get('/turmas/estatisticas', 'TurmaController@obterEstatisticas');
+        });
+    });
+
+    // Rotas de Turmas - Administrador
+    Router::grupo(['middleware' => new ControleAcessoMiddleware('ADMINISTRADOR')], function (): void {
+        Router::get('/turmas', 'TurmaController@exibirIndexAdministrador');
+        Router::get('/turmas/{id}', 'TurmaController@exibirTurmaAdministrador', ['id' => '[0-9]+']);
+        
+        Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
+            Router::get('/turmas/filtrar', 'TurmaController@filtrarTurmasAdministrador');
+            Router::get('/turmas/{id}/dados', 'TurmaController@obterTurma', ['id' => '[0-9]+']);
+            Router::get('/turmas/dados-filtros', 'TurmaController@obterDadosFiltros');
+            Router::get('/turmas/estatisticas', 'TurmaController@obterEstatisticas');
+        });
+    });
 
     // Rotas para requisições AJAX
     Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
@@ -88,6 +127,22 @@ Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void 
             Router::post('/cursos/{id}/arquivar', 'CursoController@arquivarCurso', ['id' => '[0-9]+']);
             // Router::post('/cursos/{id}/reativar', 'CursoController@reativarCurso', ['id' => '[0-9]+']);
 
+            # Matrizes Curriculares
+            Router::get('/matrizes-curriculares/obter', 'MatrizCurricularController@obterMatrizes');
+            Router::get('/matrizes-curriculares/{id}/obter', 'MatrizCurricularController@obterMatriz', ['id' => '[0-9]+']);
+            Router::get('/matrizes-curriculares/{id}/componentes/obter', 'MatrizCurricularController@obterComponentes', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/adicionar', 'MatrizCurricularController@adicionarMatriz');
+            Router::post('/matrizes-curriculares/{id}/editar', 'MatrizCurricularController@editarMatriz', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/{id}/inativar', 'MatrizCurricularController@inativarMatriz', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/{id}/validar', 'MatrizCurricularController@validarMatriz', ['id' => '[0-9]+']);
+            
+            # Componentes Curriculares
+            Router::post('/matrizes-curriculares/componentes/adicionar', 'MatrizCurricularController@adicionarComponente');
+            Router::post('/matrizes-curriculares/componentes/{id}/editar', 'MatrizCurricularController@editarComponente', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/componentes/{id}/excluir', 'MatrizCurricularController@excluirComponente', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/componentes/{id}/prerequisitos', 'MatrizCurricularController@definirPreRequisitos', ['id' => '[0-9]+']);
+            Router::post('/matrizes-curriculares/componentes/{id}/equivalencias', 'MatrizCurricularController@definirEquivalencias', ['id' => '[0-9]+']);
+
             # Usuários
             Router::get('/usuarios/buscar', 'UsuarioController@buscarUsuarios');
 
@@ -109,6 +164,19 @@ Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void 
             Router::post('/alunos/importar-sisu', 'AlunoController@importarSisu');
             Router::get('/alunos/template-sisu', 'AlunoController@baixarTemplateSisu');
 
+            # Turmas (Admin)
+            Router::get('/turmas/disciplinas', 'TurmaController@buscarDisciplinasPorCurso');
+            Router::get('/turmas/horarios', 'TurmaController@buscarHorariosPorTurno');
+            Router::post('/turmas/adicionar', 'TurmaController@adicionarTurma');
+            Router::post('/turmas/{id}/editar', 'TurmaController@editarTurma', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/arquivar', 'TurmaController@arquivarTurma', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/confirmar', 'TurmaController@confirmarTurma', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/finalizar', 'TurmaController@finalizarTurma', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/liberar', 'TurmaController@liberarTurma', ['id' => '[0-9]+']);
+            Router::get('/turmas/{id}/alunos', 'TurmaController@obterAlunos', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/alunos/adicionar', 'TurmaController@adicionarAlunos', ['id' => '[0-9]+']);
+            Router::post('/turmas/{id}/alunos/{aluno_id}/remover', 'TurmaController@removerAluno', ['id' => '[0-9]+', 'aluno_id' => '[0-9]+']);
+
         });
 
         # Grupos de Permissão
@@ -120,6 +188,10 @@ Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void 
         Router::get('/cursos', 'CursoController@exibirIndex');
         Router::get('/cursos/visualizar/{id}-{nome}', 'CursoController@exibirCurso', ['id' => '[0-9]+', 'nome' => '[a-zA-Z0-9\%][a-zA-Z0-9\-\%]*']);
 
+        # Matrizes Curriculares
+        Router::get('/matrizes-curriculares', 'MatrizCurricularController@exibirIndex');
+        Router::get('/matrizes-curriculares/visualizar/{id}', 'MatrizCurricularController@exibirMatriz', ['id' => '[0-9]+']);
+
         # Espaços
         Router::get('/espacos', 'EspacoController@exibirIndex');
 
@@ -129,18 +201,42 @@ Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void 
 
         # Alunos
         Router::get('/alunos', 'AlunoController@exibirIndex');
-        
-        # Matrizes Curriculares
-        Router::get('/matrizes', 'MatrizCurricularController@exibirIndex');
 
         # Logs
         Router::get('/logs', 'LogController@exibirIndex');
         
-        # Rotas AJAX para espaços
-        Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
-            Router::get('/espacos/{id}/dados', 'EspacoController@obterDados', ['id' => '[0-9]+']);
-        });
     });
+
+});
+
+// Rotas administrativas para inscrições (apenas ADMIN)
+Router::grupo(['middleware' => new AutenticacaoMiddleware()], function (): void {
+
+    # Inscrições (aluno)
+    Router::get('/inscricoes', 'InscricaoController@exibirIndex');
+
+    Router::grupo(['middleware' => new AjaxMiddleware()], function (): void {
+
+        # Solicitações de Inscrição
+        Router::get('/inscricoes/turmas/filtrar', 'InscricaoController@filtrarTurmas');
+        Router::post('/inscricoes/solicitar', 'InscricaoController@solicitarInscricao');
+        Router::post('/inscricoes/cancelar', 'InscricaoController@cancelarInscricao');
+
+    });
+
+
+    Router::get('/carteirinha', 'PainelController@exibirWIP'); // TODO: Implementar
+    Router::get('/curso', 'PainelController@exibirWIP'); // TODO: Implementar
+    Router::get('/disciplinas', 'PainelController@exibirWIP'); // TODO: Implementar
+    Router::get('/frequencias', 'PainelController@exibirWIP'); // TODO: Implementar
+    Router::get('/avaliacoes', 'PainelController@exibirWIP'); // TODO: Implementar
+
+    Router::get('/documentos/{tipo}', 'PainelController@exibirWIP', ['tipo' => '[a-zA-Z0-9\-]+']); // TODO: Implementar
+
+    Router::get('/turmas/arquivadas', 'PainelController@exibirWIP'); // TODO: Implementar
+
+
+    
 });
 
 Router::despachar();
