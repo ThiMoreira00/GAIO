f071fbe26f4db448039998e9fd2e2a19e0c59a5e
Thiago Moreira <129688942+ThiMoreira00@users.noreply.github.com>
2026-05-08 23:49:21 -0300

    🔧 fix: Configurações da conta

--- resources/views/auth/redefinir-senha.php
+++ resources/views/auth/redefinir-senha.php
@@ -31,7 +31,7 @@
                     </li>
                     <li id="req-special" class="flex items-center text-sm text-gray-500">
                         <svg class="h-5 w-5 mr-2 flex-shrink-0 transition-colors duration-300" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29 5.7 12.7c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.59 7.59c-.38.39-1.02.39-1.41 0z"/></svg>
-                        <span class="transition-colors duration-300">Inclusão de pelo menos um caractere especial (!@#?)</span>
+                        <span class="transition-colors duration-300">Inclusão de pelo menos um caractere especial (!@#?.)</span>
                     </li>
                 </ul>
             </div>
